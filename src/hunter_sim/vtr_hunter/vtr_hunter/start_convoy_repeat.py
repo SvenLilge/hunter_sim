e1d09e333443b4bd1c0e3eac324485c10e7f0d58
a-krawciw <freakyfactoid@gmail.com>
2025-04-22 17:36:27 -0400

    Changed ICP params to allow starting more off the path

--- src/hunter_sim/vtr_hunter/vtr_hunter/start_convoy_repeat.py
+++ src/hunter_sim/vtr_hunter/vtr_hunter/start_convoy_repeat.py
@@ -4,10 +4,10 @@ from rclpy.node import Node
 from vtr_navigation_msgs.msg import MissionCommand, GoalHandle
 
 repeat_msg = MissionCommand()     
-repeat_msg.type = MissionCommand.ADD_GOAL                                      # CHANGE
+repeat_msg.type = MissionCommand.ADD_GOAL
 repeat_msg.goal_handle = GoalHandle()
 repeat_msg.goal_handle.type = GoalHandle.REPEAT
-repeat_msg.goal_handle.waypoints = [50]
+repeat_msg.goal_handle.waypoints = [60]
 
 begin_msg = MissionCommand()
 begin_msg.type = MissionCommand.BEGIN_GOALS
@@ -17,8 +17,8 @@ class ConvoyRepeatNode(Node):
         super().__init__('convoy_repeat_node')
 
         super().__init__('minimal_publisher')
-        self.command_pub_leader = self.create_publisher(MissionCommand, '/vtr/mission_command', 10)     # CHANGE
-        self.command_pub_follower = self.create_publisher(MissionCommand, '/vtr_follower/mission_command', 10)     # CHANGE
+        self.command_pub_leader = self.create_publisher(MissionCommand, '/vtr/mission_command', 10)
+        self.command_pub_follower = self.create_publisher(MissionCommand, '/vtr_follower/mission_command', 10) 
         timer_period = 1.0
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
@@ -28,7 +28,7 @@ class ConvoyRepeatNode(Node):
         if self.i < len(self.messages):
             self.command_pub_leader.publish(self.messages[self.i])
             self.command_pub_follower.publish(self.messages[self.i])
-            self.get_logger().info('Publishing:')  # CHANGE
+            self.get_logger().info('Publishing:')
             self.i += 1
         else:
             sys.exit(0)
